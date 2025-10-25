4c045e084eab82e7f235bacf8641cf39ea9b1e1a
Harshit Pal <harshitpalpb@gmail.com>
2025-10-25 21:43:18 +0530

    Newt word prediction

--- app.py
+++ app.py
@@ -4,31 +4,29 @@ import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-#Load the LSTM Model
-model=load_model('next_word_lstm.h5')
+@st.cache_resource
+def load_lstm():
+    model = load_model('next_word_lstm.h5')
+    with open('tokenizer.pickle','rb') as handle:
+        tok = pickle.load(handle)
+    rev = {v:k for k,v in tok.word_index.items()}
+    return model, tok, rev
 
-#3 Laod the tokenizer
-with open('tokenizer.pickle','rb') as handle:
-    tokenizer=pickle.load(handle)
+model, tokenizer, rev_index = load_lstm()
 
-# Function to predict the next word
 def predict_next_word(model, tokenizer, text, max_sequence_len):
-    token_list = tokenizer.texts_to_sequences([text])[0]
-    if len(token_list) >= max_sequence_len:
-        token_list = token_list[-(max_sequence_len-1):]  # Ensure the sequence length matches max_sequence_len-1
-    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-    predicted = model.predict(token_list, verbose=0)
-    predicted_word_index = np.argmax(predicted, axis=1)
-    for word, index in tokenizer.word_index.items():
-        if index == predicted_word_index:
-            return word
-    return None
+    tok = tokenizer.texts_to_sequences([text])[0]
+    if len(tok) >= max_sequence_len:
+        tok = tok[-(max_sequence_len-1):]
+    tok = pad_sequences([tok], maxlen=max_sequence_len-1, padding='pre')
+    pred = model.predict(tok, verbose=0)
+    idx = int(np.argmax(pred, axis=1))
+    return rev_index.get(idx)
 
-# streamlit app
 st.title("Next Word Prediction With LSTM And Early Stopping")
-input_text=st.text_input("Enter the sequence of Words","To be or not to")
-if st.button("Predict Next Word"):
-    max_sequence_len = model.input_shape[1] + 1  # Retrieve the max sequence length from the model input shape
-    next_word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
-    st.write(f'Next word: {next_word}')
 
+input_text = st.text_input("Enter words", "To be or not to")
+if st.button("Predict Next Word"):
+    max_sequence_len = model.input_shape[1] + 1
+    word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
+    st.write(f"Next word: {word}")
